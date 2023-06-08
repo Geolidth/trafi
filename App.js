205bc1b3e84c5899e614f1a650e41cf270794f20
Geolidth <myyouz74@gmail.com>
2023-06-08 19:32:15 +0200

    Kész

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './components/Button';
+//import { Button } from 'react-native-web';
 
 export default function App() {
+  const [count,setCunter]= useState(0);
+  function increment(){
+    let result =count + 1;
+    setCunter(result);
+  }
+
+  function decrement(){
+    let result = count - 1;
+    setCunter(result);
+  }
+
+  function reset(){
+    let result =0;
+    setCunter(result);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Forgalom</Text>
+      <Text style={styles.text}>számláló</Text>
+      <Text style={styles.count}>{count}</Text>
+
+      <Button title='Növel' 
+      onPress={increment}  />
+
+      <Button title='Csökkent' 
+      onPress={decrement}  />
+
+      <Button title='Reset' 
+      onPress={reset}  />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +43,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'royalblue',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text:{
+    color:'coral',
+    fontSize:32,
+    fontFamily:'monospace',
+  },
+  count:{
+    fontSize:44,
+    color:'violet'
   },
 });
